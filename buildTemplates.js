687b532ca9515e2fe802be6728cd895663a6a28d
Tolga Atam <30860371+tolgaatam@users.noreply.github.com>
2018-11-20 06:06:13 +0300

    Update buildTemplates.js

--- buildTemplates.js
+++ buildTemplates.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const source = __dirname + "/views";
 const destination = __dirname + "/.dotjs_build/";
 
-if (!fs.existsSync(destination)){
+if (!fs.existsSync(destination)){ // if the destination folder is not present, make one first
     fs.mkdirSync(destination);
 }
 
-require("dot").process({
+require("dot").process({ // compile the templates into the destination folder.
     destination: destination,
     path: source
 });
